b7451c06e1cc992c0b892d75f1486516a281c3f7
Veena S <veenasriramoju@gmail.com>
2026-01-15 10:33:49 +0000

    Fix static files loading on Heroku

--- only_studies/settings.py
+++ only_studies/settings.py
@@ -141,6 +141,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# WhiteNoise configuration for efficient static file serving
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
